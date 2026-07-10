d556e50e8288526eb828eccf5ff497daec805057
black <black@example.com>
2026-07-10 16:21:08 +0500

    Change greeting to 'hello everyone'

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         // Оставляем старый вывод, чтобы было видно разницу
-        Console.WriteLine("hello");
+        Console.WriteLine("hello everyone");
     }
 
     // Новый метод — приветствие от White
